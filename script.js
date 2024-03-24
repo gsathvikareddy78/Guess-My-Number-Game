7bf22d8637cedc64b87fd2303ff663129546a799
G.Sathvika <153293504+gsathvikareddy78@users.noreply.github.com>
2024-03-24 11:53:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ let secretNumber=Math.trunc(Math.random()*20+1);
 console.log(secretNumber);
 let score=20;
 let highscore=0;
+
+// logic
+document.getElementById('checkBtn').addEventListener('click',()=>{
+    const guessedNo=Number(document.getElementById('guess').value);
+    console.log(guessedNo);
+
+    // if there is no input:
+    if(!guessedNo){
+        document.querySelector(".message").textContent="No Number!";
+    }
+
+    // when player wins:
+    else if(guessedNo===secretNumber){
+        document.querySelector(".message").textContent="Correct Number!";
+        document.querySelector(".number").textContent=secretNumber;
+    }
+})
